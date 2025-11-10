6709bd39109015a18d861046ba50d11c617f0ad0
amandrarry <amandaverdugoacosta@gmail.com>
2025-11-10 15:21:29 +0100

    models.py

--- lista_tareas/models/models.py
+++ lista_tareas/models/models.py
@@ -7,8 +7,7 @@ class lista_tareas(models.Model):
     _description = 'lista_tareas.lista_tareas'
     #Elementos de cada fila del modelo de datos
     #Los tipos de datos a usar en el ORM son
-    #
-    https://www.odoo.com/documentation/17.0/developer/reference/addons/orm.html#fields
+    # https://www.odoo.com/documentation/17.0/developer/reference/addons/orm.html#fields
     tarea = fields.Char()
     prioridad = fields.Integer()
     urgente = fields.Boolean(compute="_value_urgente", store=True)
